d93ff1bc4e8b594fdbbc3a3c1a3b4751aa035c78
PituPitu0 <PituPitu0@users.noreply.github.com>
2024-01-27 21:19:51 +0100

    27.01.2024.04

--- src/components/FeatureNews/index.js
+++ src/components/FeatureNews/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function FeatureNews() {
   const [articles, setArticles] = useState([]);
@@ -11,7 +12,6 @@ function FeatureNews() {
   const fetchArticles = async () => {
     try {
       const response = await axios.get('https://strapi-dt37.onrender.com/api/newss?populate=*');
-      
       setArticles(response.data.data);
     } catch (error) {
       console.log('Wystąpił błąd:', error);
@@ -24,9 +24,8 @@ function FeatureNews() {
         <div key={article.id}>
           <div className="widget-title border-radious5 mb30 shadow7 padding20">
             <h1>
-              <a href={`/post${article.id}`}>{article.attributes.title}</a>
-              {/* <a href={`/post1/${article.id}`}>{article.attributes.title}</a> */}
-              {/* ver oryginal ^^ */}
+              {/* Zamiast <a href={`/post${article.id}`}> */}
+              <Link to={`/post${article.id}`}>{article.attributes.title}</Link>
             </h1>
           </div>
         </div>
