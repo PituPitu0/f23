d7661e11a95e0af281130ce3eb526633f158135c
PituPitu0 <PituPitu0@users.noreply.github.com>
2024-01-28 14:14:20 +0100

    28.01.2024.02

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,82 +1,80 @@
-/* eslint-disable no-unused-vars */
-import React, {Fragment} from 'react';
-
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 import FeatureNews from "../../components/FeatureNews";
-
-
+import BannerSection from "../../components/BannerSection";
 
 const HomePage = () => {
-    return (
-      
-        <div className="container">
+  const [featureNews, setFeatureNews] = useState([]);
+
+  useEffect(() => {
+    fetchFeatureNews();
+  }, []);
+
+  const fetchFeatureNews = async () => {
+    try {
+      const response = await axios.get('https://strapi-dt37.onrender.com/api/newss?populate=*');
+      setFeatureNews(response.data.data);
+    } catch (error) {
+      console.log('Wystąpił błąd:', error);
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="container">
         <Fragment>
-            {/*<PostCarousel className="fifth_bg"/>*/}
-            {/*<PostGallery className="fifth_bg"/>*/}
-            <FeatureNews/>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8">
-                    {/*  <TrendingNews/>*/}
-                    </div>
-                    <div className="col-md-12 col-lg-4">
-                       {/*  <FollowUs title="Follow Us"/>*/}
-                   {/*   <MostView/>*/} 
-                    </div>
-                </div>
-            </div>
-         {/* <MixCarousel className="half_bg1"/>*/} 
-            
-            <div className="entertrainments">
-                <div className="container">
+          <FeatureNews featureNews={featureNews} />
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8">
+                <div className="entertrainments">
+                  <div className="container">
                     <div className="row">
-                        <div className="col-lg-8">
-                            <div className="row">
-                                <div className="col-12">
-                                                               </div>
-                            </div>
-                            {/*CAROUSEL START*/}
-                            <div className="entertrainment_carousel mb30">
-                                <div className="entertrainment_item">
-                                    <div className="row justify-content-center">
-                                  
-                                    </div>
-                                </div>
-                            </div>
-                            {/*CAROUSEL END*/}
-                         {/*    <SportsNews/>
-                            <div className="banner_area mt50 mb60 xs-mt60">
-                                <Link to="/">
-                                    <img src={banner1} alt="banner1"/>
-                                </Link>
-                            </div>
-                          {/*  <LocalNews localNews={localNews}/>*/}
+                      <div className="col-lg-8">
+                        <div className="row">
+                          <div className="col-12">
+                            {/* Pozostała część komponentu */}
+                          </div>
                         </div>
-                        <div className="col-lg-4">
-                            <div className="row">
-                                <div className="col-lg-12">
-                              {/*      <MostShareWidget title="Most share"/>*/} 
-                                </div>
-                                <div className="col-lg-12">
-                                 {/*   <UpcomingMatches/>*/}
-                                </div>
-                            
-                                <div className="col-lg-12">
-                            {/*  <CategoriesWidget/>*/} 
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="banner2 mb30">
-                                     
-                                    </div>
-                                </div>
+                        {/*CAROUSEL START*/}
+                        <div className="entertrainment_carousel mb30">
+                          <div className="entertrainment_item">
+                            <div className="row justify-content-center">
+                              {/* Pozostała część komponentu */}
                             </div>
+                          </div>
                         </div>
+                        {/*CAROUSEL END*/}
+                        {/* Pozostała część komponentu */}
+                      </div>
+                      <div className="col-lg-4">
+                        <div className="row">
+                          <div className="col-lg-12">
+                            {/* Pozostała część komponentu */}
+                          </div>
+                          <div className="col-lg-12">
+                            {/* Pozostała część komponentu */}
+                          </div>
+                          <div className="col-lg-12">
+                            {/* Pozostała część komponentu */}
+                          </div>
+                          <div className="col-lg-12">
+                            {/* Pozostała część komponentu */}
+                          </div>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-            <div className="space-70"/>
+          </div>
         </Fragment>
-        </div>
-    );
+      </div>
+      <div className="space-70"/>
+      <BannerSection/>
+    </Fragment>
+  );
 };
 
 export default HomePage;
