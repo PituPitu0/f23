612dfb05a3d8cd6f84c6e32260c6bbd468256be7
PituPitu0 <PituPitu0@users.noreply.github.com>
2024-03-04 21:28:46 +0100

    04.03.2024.01

--- src/components/FeatureNews/index.js
+++ src/components/FeatureNews/index.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 function FeatureNews() {
   const [articles, setArticles] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [articlesPerPage] = useState(3);
 
   useEffect(() => {
     fetchArticles();
@@ -12,8 +14,6 @@ function FeatureNews() {
   const fetchArticles = async () => {
     try {
       const response = await axios.get('https://strapi-dt37.onrender.com/api/newss?populate=*');
-      // Odwracanie kolejności tablicy przed przypisaniem do stanu, 
-      // aby uniknąć odwracania przy każdym renderowaniu
       const reversedArticles = response.data.data.reverse();
       setArticles(reversedArticles);
     } catch (error) {
@@ -21,17 +21,66 @@ function FeatureNews() {
     }
   };
 
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  const currentArticles = articles.slice(indexOfFirstArticle, indexOfLastArticle);
+
+  const totalPages = Math.ceil(articles.length / articlesPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const nextPage = () => {
+    setCurrentPage(currentPage => Math.min(currentPage + 1, totalPages));
+  };
+
+  const prevPage = () => {
+    setCurrentPage(currentPage => Math.max(currentPage - 1, 1));
+  };
+
   return (
     <div>
-      {articles.map((article) => (
+      {currentArticles.map((article) => (
         <div key={article.id}>
           <div className="widget-title border-radious5 mb30 shadow7 padding20">
             <h1>
               <Link to={`/post${article.id}`}>{article.attributes.title}</Link>
             </h1>
+        {/*  <div className="author_about author_wrap author_img" >
+            <Link to={`/post${article.id}`}>
+                    {/* Użycie danych o obrazie, sprawdzenie, czy artykuł ma obraz */}
+                   {/* {article.attributes.image && (
+                      <img 
+                        src={`https://strapi-dt37.onrender.com${article.attributes.image.data.attributes.formats.thumbnail.url}`}
+                        alt={article.attributes.image.name}
+                      />
+                  
+                    )}
+                  </Link>
+              </div> */}
           </div>
         </div>
       ))}
+      <nav>
+        <ul className='pagination'>
+          <li className='page-item'>
+            <a onClick={prevPage} href='#!' className='page-link'>
+              &laquo;
+            </a>
+          </li>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <li key={index} className={currentPage === index + 1 ? 'page-item active' : 'page-item'}>
+              <a onClick={() => paginate(index + 1)} href='#!' className='page-link'>
+                {index + 1}
+              </a>
+            </li>
+          ))}
+          <li className='page-item'>
+            <a onClick={nextPage} href='#!' className='page-link'>
+              &raquo;
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
