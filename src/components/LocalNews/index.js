f3f1fa60e371b0e6397aa5781eccc266ad492424
PituPitu0 <PituPitu0@users.noreply.github.com>
2024-03-18 13:16:33 +0100

    18.03.2024.01

--- src/components/LocalNews/index.js
+++ src/components/LocalNews/index.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 
@@ -8,66 +8,72 @@ const LocalNews = ({ localNews, currentPage, onPageChange }) => {
   const displayedNews = localNews.slice(offset, offset + articlesPerPage);
 
   const truncateText = (text, maxLength) => {
-    return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
-  };
+    if (text.length <= maxLength) {
+      return text;
+    }
 
+    let fragment = text.substring(0, maxLength); 
+
+    
+    let lastDotIndex = fragment.lastIndexOf('.');
+    if (lastDotIndex >= 90 && lastDotIndex <= maxLength) {
+      return fragment.substring(0, lastDotIndex + 1);
+    } else {
+      
+      let lastSpaceIndex = fragment.lastIndexOf(' ', maxLength);
+      if (lastSpaceIndex !== -1) {
+        return fragment.substring(0, lastSpaceIndex) + '...'; 
+      } else {
+        return text.substring(0, maxLength) + '...'; 
+      }
+    }
+  };
 
   return (
     <div className="row">
       <div className="col-12">
         <div className="businerss_news">
-         <div className="row">
-          <div className="col-12">
-            <div className="single_post post_type3 post_type12 mb30">
-              {displayedNews.map((article) => (
-            <div key={article.id}>
-              <div className="post_img">
-                <div className="img_wrap">
-                  <Link to={`/post${article.id}`}>
-                    {/* Użycie danych o obrazie, sprawdzenie, czy artykuł ma obraz */}
-                    {article.attributes.image && (
-                      <img
-                        src={`https://strapi-dt37.onrender.com${article.attributes.image.data.attributes.formats.thumbnail.url}`}
-                        alt={article.attributes.image.name}
-                      />
-                  
-                    )}
-                  </Link>
-                </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="single_post post_type3 post_type12 mb30">
+                {displayedNews.map((article) => (
+                  <div key={article.id}>
+                    <div className="post_img">
+                      <div className="img_wrap">
+                        <Link to={`/post${article.id}`}>
+                          {article.attributes.image && (
+                            <img
+                              src={`https://strapi-dt37.onrender.com${article.attributes.image.data.attributes.formats.thumbnail.url}`}
+                              alt={article.attributes.image.name}
+                            />
+                          )}
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="single_post_text">
+                      <h4>
+                        <Link to={`/post${article.id}`}>{article.attributes.title}</Link>
+                      </h4>
+                      <div>
+                        <p className="post-p">
+                          {truncateText(article.attributes.article, 100)}
+                        </p>
+                      </div>
+                      <div className="space-20" />
+                      <div>
+                        <Link className="readmore" to={`/post${article.id}`}>
+                          czytaj dalej
+                        </Link>
+                      </div>
+                      <div className="space-20" />
+                      <div className="border_black" />
+                      <div className="space-20" />
+                    </div>
+                  </div>
+                ))}
               </div>
-              
-              <div className="single_post_text">
-              
-                <h4>
-                  <Link to={`/post${article.id}`}>{article.attributes.title}</Link>
-                </h4>
-               
-                <div>
-                  {/* Wyświetl tylko 30 znaków z całego artykułu */}
-                  <p className="post-p">
-                    {truncateText(article.attributes.article, 100)}
-                  </p>
-                </div>
-                <div className="space-20" />
-                <div>
-          
-                  <Link className="readmore" to={`/post${article.id}`}>
-                    czytaj dalej
-                  </Link>
-                </div>
-                <div className="space-20" />
-                <div className="border_black" />
-                <div className="space-20" />
-              </div>
-             
             </div>
-            
-            
-  
-          ))}
           </div>
-          </div>
-        </div>
         </div>
       </div>
       <ReactPaginate
